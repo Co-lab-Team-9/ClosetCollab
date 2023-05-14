26bec153fceef972990cc5cc2e78c05306a50e49
Hemant <hrsmart2150@gmail.com>
2023-05-14 15:10:49 +0530

    Setted up our app

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-blue-800 text-6xl">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa doloremque,
+      illum, blanditiis repellat dignissimos temporibus minima fugiat quidem,
+      repudiandae natus saepe. Sint sapiente vero facere magnam numquam,
+      molestias commodi, corporis, reiciendis assumenda hic est.
+    </div>
+  );
+}
+
+export default App;
