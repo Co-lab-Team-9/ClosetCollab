b3ef2a636787b0340bb1d600110350a4217d5717
mitalrs <mitalsapkale1999@gmail.com>
2023-05-22 15:14:17 +0530

    server runing properly on 4040 port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("structure work perfectly");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const  PORT = 4040;
+
+// app.use("/api/v1", routes)
+
+
+app.listen(PORT, () => {
+    console.log('runnig', PORT)
+  })
