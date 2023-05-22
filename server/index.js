be097399d95a01dcf56fdf9e52949756d7c82f39
mitalrs <mitalsapkale1999@gmail.com>
2023-05-22 22:59:09 +0530

    file structure

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const  PORT = 4040;
+const  PORT = process.env.PORT || 4040;
 
 // app.use("/api/v1", routes)
+// app.get("/api/contacts", (req,res)=>{
+//   res.json({message: "get out" })
+// })
 
 
 app.listen(PORT, () => {
